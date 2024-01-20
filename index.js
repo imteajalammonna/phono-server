b574050e5812a4191b1623a5c478e592a8c9a08c
imteajalammonna <imteajalammonna@gmail.com>
2024-01-21 02:48:58 +0600

    phones data loaded

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-  
+app.use(cors());
+app.use(express.json());
 
 
 
